56b02c2430d4a709f10f65d46424ba38510a2e54
Tomaz Medrado <tmedrado99@gmail.com>
2022-03-20 11:59:47 -0300

    next lint

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import { Container, Typography } from '@material-ui/core';
-import { useRouter } from 'next/router';
 
 function Home() {
-  const router = useRouter();
   return (
     <Container>
       <Typography mx={3} variant="h2" component="div">

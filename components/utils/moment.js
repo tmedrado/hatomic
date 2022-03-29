6de9c2823ae939f4c59c780fe9ec6edf8c3a26ff
Tomaz Medrado <tmedrado99@gmail.com>
2022-03-29 09:29:04 -0300

    deploy

--- components/utils/moment.js
+++ components/utils/moment.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-import moment from 'moment';
+import moment from 'moment'
 
-export const currentMonth = moment().format('MMM');
+export const currentMonth = moment().format('MMM')

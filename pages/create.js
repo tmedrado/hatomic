b8dcfd49787eb9750b1828fc94de9900a3e27934
Tomaz Medrado <tmedrado99@gmail.com>
2022-03-29 22:15:53 -0300

    fix link

--- pages/create.js
+++ pages/create.js
@@ -24,7 +24,7 @@ const HabitForm = () => {
   const handleCreateHabit = (e) => {
     e.preventDefault()
 
-    fetch('http://hatomic-git-master-tmedrado.vercel.app/create', {
+    fetch('http://hatomic-git-master-tmedrado.vercel.app/api/create', {
       method: 'POST',
       body: JSON.stringify({ formData }),
     })
